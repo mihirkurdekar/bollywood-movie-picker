b0a2a90f1eedec8dc7487d2c2f7d21199144d5b1
mihirkurdekar <mihir.kurdear@gmail.com>
2025-09-07 03:11:57 +0530

    Update runtime python 3.11

--- bollywood_movie_picker/app.py
+++ bollywood_movie_picker/app.py
@@ -1,13 +1,13 @@
-from jinja2 import Environment, FileSystemLoader
 from flask import Flask
 import pandas as pd
 
 app = Flask(__name__)
-environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("movie_view.html")
 
-# Load movies from parquet file
-movies = pd.read_csv('static/movies_parquet.csv.gz',compression='gzip')
+# Load movies from compressed CSV file
+def load_movies():
+    return pd.read_csv('static/movies_parquet.csv.gz', compression='gzip')
+
+movies = load_movies()
 
 @app.route('/')
 def hello_world():
@@ -15,7 +15,37 @@ def hello_world():
     title = movie['title'].values[0]
     title_id = movie['titleId'].values[0]
     imdb_url = f"https://www.imdb.com/title/{title_id}/"
-    return template.render(imdb_url=imdb_url, name=title)
+    html = f"""
+    <!doctype html>
+    <html lang='en'>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>Bollywood Movie Picker</title>
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet'>
+    </head>
+    <header class='container'>
+        <nav class='navbar navbar-light bg-light'>
+            <h1>Bollywood Movie Picker!</h1>
+        </nav>
+    </header>
+    <body>
+    <div class='container'>
+        <p> Movie: <a href='{imdb_url}' target='_blank'>{title}</a></p>
+        <button onclick='window.location.reload()'>Refresh Movie</button>
+    </div>
+    <div class='container'>
+        <p>This app is developed for playing Dumb Charades or pictionary kind of games.</p>
+        <b>Happy guessing!</b>
+    </div>
+    </body>
+    <footer class='container'>
+        <p>Reference from list: <a href='https://www.imdb.com/search/title/?title_type=feature&languages=hi' target='_blank'>imdb filtered list</a></p>
+        <p>Code reference: <a target='_blank' href='https://github.com/mihirkurdekar/bollywood-movie-picker/blob/master/Bollywood_Dumb_Charades.ipynb'>Mihir's Jupyter Notebook</a></p>
+    </footer>
+    </html>
+    """
+    return html
 
 if __name__ == '__main__':
     app.run()
